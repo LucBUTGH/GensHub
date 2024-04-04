9da07367413b113e88a6e20d61a572402294ceda
LucBUTGH <lucleveque14@outlook.fr>
2024-04-04 23:50:42 +0200

    feat: fetch datas from GenshinAPI

---
+++ src/views/APIView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Getting datas from localhost api</h1>
+  <div v-for="item in items" :key="item.char_id">
+    <p>{{ item.cha_name }}</p>
+  </div>
+  <p v-if="error">Erreur lors de la récupération des données: {{ error }}</p>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      items: [],
+      error: ''
+    }
+  },
+  async created() {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/characters')
+      this.items = response.data.data
+    } catch (error) {
+      this.error = error.toString()
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,19 @@ import { supabase } from '@/lib/supabase'
 export default {
   setup() {
     const characters = ref([])
+    const weapons = ref([])
+    const artifacts = ref([])
     const errorMessage = ref(null)
 
     const fetchCharacters = async () => {
       try {
-        const { data, error } = await supabase.from('gen_characters').select('*')
+        const { data, error } = await supabase.from('gen_characters').select('*').order('cha_id')
 
-        if (error) throw error
+        if (error) {
+          console.log('error', error)
+        } else {
+          console.log('data', data)
+        }
 
         characters.value = data
       } catch (err) {
@@ -19,10 +25,46 @@ export default {
       }
     }
 
+    const fetchWeapons = async () => {
+      try {
+        const { data, error } = await supabase.from('gen_weapons').select('*')
+
+        if (error) {
+          console.log('error', error)
+        } else {
+          console.log('data', data)
+        }
+
+        weapons.value = data
+      } catch (err) {
+        errorMessage.value = err.message
+      }
+    }
+
+    const fetchArtifacts = async () => {
+      try {
+        const { data, error } = await supabase.from('gen_artifacts').select('*')
+
+        if (error) {
+          console.log('error', error)
+        } else {
+          console.log('data', data)
+        }
+
+        artifacts.value = data
+      } catch (err) {
+        errorMessage.value = err.message
+      }
+    }
+
     onMounted(fetchCharacters)
+    onMounted(fetchWeapons)
+    onMounted(fetchArtifacts)
 
     return {
       characters,
+      weapons,
+      artifacts,
       errorMessage
     }
   }
@@ -32,9 +74,22 @@ export default {
 <template>
   <h1>Test getting datas!</h1>
   <p v-if="errorMessage">{{ errorMessage }}</p>
+  <h1>Personnages</h1>
   <ul>
     <li v-for="character in characters" :key="character.cha_id">
       {{ character.cha_name }}
+    </li>
+  </ul>
+  <h1>Artéfacts</h1>
+  <ul>
+    <li v-for="artifact in artifacts" :key="artifact.art_id">
+      {{ artifact.art_label }}
+    </li>
+  </ul>
+  <h1>Weapons</h1>
+  <ul>
+    <li v-for="weapon in weapons" :key="weapon.wea_id">
+      {{ weapon.wea_name }}
     </li>
   </ul>
 </template>
